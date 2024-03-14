bfb51bdb1b047edf86c09f203ac257b3fe3ce4c6
k1skac <110919738+k1skac@users.noreply.github.com>
2024-03-14 18:30:25 +0100

    Add base page, home page a,d post page with django template

---
+++ feed/views.py
@@ -0,0 +1,36 @@
+from typing import Any
+from django.http import HttpResponse
+from django.views. generic import TemplateView, DetailView, FormView
+from .models import Post 
+from .forms import PostForm
+from django.contrib import messages
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all().order_by('-id')
+        return context
+
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
+
+class AddPostView(FormView):
+    template_name = "new_post.html"
+    form_class = PostForm
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs )
+
+    def form_valid(self, form: Any) -> HttpResponse:
+        new_Post = Post.objects.create(
+            text = form.cleaned_data['text'],
+            image = form.cleaned_data['image']
+        )
+        messages.add_message(self.request, messages.SUCCESS, "Your post was successful")
+        return super().form_valid(form)
